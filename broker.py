14c5838ab8de5e1f78228a75986d7960fc66e4ad
BeatrizLdS <beatriz_l.silva@hotmail.com>
2024-03-06 00:44:30 -0300

    :spakles: feat: Connect with clients and send messages

--- broker.py
+++ broker.py
@@ -1,5 +1,12 @@
 import socket
 
+def get_server_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # Socket UDP
+    s.connect(("8.8.8.8", 80))  # Connect to Google DNS Server
+    server_ip = s.getsockname()[0]  # Get real local IP
+    s.close()
+    return server_ip
+
 # Socket UDP
 discovery_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 discovery_address = ('0.0.0.0', 1050)
@@ -15,14 +22,19 @@ while True:
     # Server register
     if data == b'REGISTER_SERVER': 
         server_info = client_address[0]
-        available_servers[client_address] = server_info
-        print(f"Registered server: {client_address}")
+        if server_info == '127.0.0.1':
+            server_info = get_server_ip()
+            available_servers[client_address] = server_info
+            print(f"Registered server: {server_info}")
+        else:
+            available_servers[client_address] = server_info
+            print(f"Registered server: {server_info}")
         
     # Client request
     elif data == b'REQUEST_SERVER': 
         if available_servers:      
             for server_address, server_info in available_servers.items():
-                print(server_address)
+                print(client_address)
                 discovery_socket.sendto(server_info.encode('utf-8'), client_address)
         else:
             discovery_socket.sendto(b'NO_SERVERS_AVAILABLE', client_address)
